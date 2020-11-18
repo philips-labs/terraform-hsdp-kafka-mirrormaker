93a182a28f7e558822cbe846e9821eb6106f148a
Marco Westerink <marco.westerink@philips.com>
2020-11-18 07:59:52 +0100

    Fix mm_keystore naming

--- containter_host.tf
+++ containter_host.tf
@@ -64,7 +64,7 @@ resource "null_resource" "cluster" {
 
   provisioner "file" {
     source      = var.key_store.keystore
-    destination = "/home/${var.user}/mm_keystore.jks"
+    destination = "/home/${var.user}/mm.keystore.jks"
   }
   
   provisioner "file" {
